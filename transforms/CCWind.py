c1e4731c71ab2d2faa4d34bc313b77a205e748f0
watashi-Gent <105342160+watashi-Gent@users.noreply.github.com>
2024-09-18 15:57:27 +0900

    Update CCWind.py

--- transforms/CCWind.py
+++ transforms/CCWind.py
@@ -4,6 +4,16 @@ from PIL import Image, ImageFile
 
 # CENTER CROP WINDOW OF AN IMAGE
 def CCWind(self, img, size):
+    """
+    A window to center-crop a given image.
+
+    Args:
+        img: Input image.
+        size: The side lenth of the center-squared image.
+
+    Returns:
+        img.crop((left, top, right, bottom)): The center-squared image.
+    """
     width, height = img.size
     left = (width - size[0]) / 2
     top = (height - size[1]) / 2
