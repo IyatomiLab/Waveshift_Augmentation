f7d83a3f64f5ee5172c6ee5d78acc1bfea90a0e5
watashi-Gent <105342160+watashi-Gent@users.noreply.github.com>
2025-03-30 16:19:02 +0900

    Update __init__.py

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -2,7 +2,8 @@
 from .CCWind import CCWind
 from .FT2Dc import FT2Dc
 from .IFT2Dc import IFT2Dc
-from .Propagator import Propagator
+from .PropagatorS import PropagatorS
+from .PropagatorPSF import PropagatorPSF
 
-__all__ = ['CCWind', 'FT2Dc', 'IFT2Dc', 'Propagator']
+__all__ = ['CCWind', 'FT2Dc', 'IFT2Dc', 'PropagatorS', 'PropagatorPSF']
 
