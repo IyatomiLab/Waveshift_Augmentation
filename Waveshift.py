349c13d1b1f0b010fbf3c37f69152b32d4780cf4
watashi-Gent <105342160+watashi-Gent@users.noreply.github.com>
2025-03-30 16:26:01 +0900

    Update Waveshift.py

--- Waveshift.py
+++ Waveshift.py
@@ -10,7 +10,9 @@ class Wavefront_Shift():
                 lambdaRED = 620 * 10**(-9),
                 lambdaGREEN = 535 * 10**(-9),
                 lambdaBLUE = 450 * 10**(-9),
-                upper_bound = 41):
+                upper_bound = 41,
+                aperture_coeff = 0.01,
+                mode_="s"):
 
         """
         Initializes our proposed augmentation transform.
@@ -20,12 +22,15 @@ class Wavefront_Shift():
             lambdaGREEN (float, fixed): The theoretical wavelength for the green channel. Standard is 535 * 10**(-9).
             lambdaBLUE (float, fixed): The theoretical wavelength for the blue channel. Standard is 450 * 10**(-9).
             upper_bound (int, optional): The maximum distance z0 where the wavefront will be shifted at.
+            mode_ (string, optional): Use mode_ t0 change between WS 1.0 ("s" - one hyperparameter) and WS 2.0 ("psf" - two hyperparameters).
         """
         # PARAMETERS
         self.lambdaRED = lambdaRED
         self.lambdaGREEN = lambdaGREEN
         self.lambdaBLUE = lambdaBLUE
         self.upper_bound = upper_bound
+        self.aperture_coeff = aperture_coeff
+        self.mode_ = mode_
     
     def __call__(self, leaf):
         columns, rows = leaf.size
@@ -46,10 +51,20 @@ class Wavefront_Shift():
         # BLUE = CCWind(blueChannel, (self.Np, self.Np))    
         BLUE = blueChannel                  
       
-        # Precompute all Propagator for all the range of Z
-        propRED = Propagator(self.Nx, self.Ny, self.lambdaRED, self.z0) 
-        propGREEN = Propagator(self.Nx, self.Ny, self.lambdaGREEN, self.z0 )
-        propBLUE = Propagator(self.Nx, self.Ny, self.lambdaBLUE, self.z0 ) 
+        if mode_ == "s":
+            #######################################################################
+            # Precompute all Propagators for all the range of Z
+            propRED = self.PropagatorS(self.Nx, self.Ny, self.lambdaRED, self.z0) 
+            propGREEN = self.PropagatorS(self.Nx, self.Ny, self.lambdaGREEN, self.z0 )
+            propBLUE = self.PropagatorS(self.Nx, self.Ny, self.lambdaBLUE, self.z0 ) 
+
+        elif mode_ == "psf":
+            #######################################################################
+            # Precompute all Propagators for all the range of Z
+            propRED = self.PropagatorPSF(self.Nx, self.Ny, self.lambdaRED, self.z0, self.aperture_coeff) 
+            propGREEN = self.PropagatorPSF(self.Nx, self.Ny, self.lambdaGREEN, self.z0, self.aperture_coeff)
+            propBLUE = self.PropagatorPSF(self.Nx, self.Ny, self.lambdaBLUE, self.z0, self.aperture_coeff)
+ 
 
         # Propagate the color channels for the respective leaf
         recRED = np.abs(IFT2Dc(FT2Dc(RED) * propRED))
